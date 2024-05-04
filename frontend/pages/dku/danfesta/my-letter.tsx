fcb12fd9fc6ef83cd4e533eb9f74b93d3977b059
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-04 13:09:30 +0900

    feat: 단국대 이벤트 페이지 마크업

---
+++ frontend/pages/dku/danfesta/my-letter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyLetter = () => {
+  return <div>MyLetter</div>;
+};
+
+export default MyLetter;
--- frontend/pages/dku/signin.tsx
+++ frontend/pages/dku/signin.tsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import { Frame } from "../../styles/components/Frame";
 import Head from "next/head";
+import dynamic from "next/dynamic";
+// import DankookStudentCouncilLogin from "dankook-student-council-login";
+const DankookStudentCouncilLogin = dynamic(
+  () => import("dankook-student-council-login"),
+  {
+    ssr: false,
+  }
+);
+
 function SignIn() {
   const signIn = () => {};
 
@@ -9,6 +18,16 @@ function SignIn() {
       <Head>
         <title>로그인 - Text me!</title>
       </Head>
+      <DankookStudentCouncilLogin
+        clientId="dku-text-me"
+        onSuccess={(res) => {
+          console.log(res);
+        }}
+        onError={(err) => {
+          console.log(err);
+        }}
+      />
+      <div style={{ background: "red" }}>안녕</div>
     </Frame>
   );
 }
