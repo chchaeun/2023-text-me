b3186220cd45fe3a870dcd7142cc13612dc93a8a
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-04 13:10:26 +0900

    feat: 신고 기능 추가

--- frontend/components/room/LetterViewContainer.tsx
+++ frontend/components/room/LetterViewContainer.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
-import { useLetterView } from '../../stores/useLetterView';
-import { Overlay } from '../../styles/components/Modal';
-import LetterView from './LetterView';
-import LetterViewMove from './LetterViewMove';
+import React, { useEffect } from "react";
+import { useLetterView } from "../../stores/useLetterView";
+import { Overlay } from "../../styles/components/Modal";
+import LetterView from "./LetterView";
+import LetterViewMove from "./LetterViewMove";
+import ReportIcon from "../common/icons/ReportIcon";
+import styled from "styled-components";
+import { useAlertModal } from "../../stores/useAlertModal";
 
 function LetterViewContainer() {
   const { isOpened, id, getLetter, close } = useLetterView();
+
+  const { openAlertModal } = useAlertModal();
 
   useEffect(() => {
     if (id) {
@@ -18,13 +23,39 @@ function LetterViewContainer() {
   }
 
   return (
-    <div>
-      <Overlay onClick={close} />
-      <LetterViewMove>
-        <LetterView />
-      </LetterViewMove>
-    </div>
+    <>
+      <Report
+        type="button"
+        onClick={() => openAlertModal("편지를 신고하시겠습니까?")}
+      >
+        신고
+        <ReportIcon />
+      </Report>
+      <div>
+        <Overlay onClick={close} />
+        <LetterViewMove>
+          <LetterView />
+        </LetterViewMove>
+      </div>
+    </>
   );
 }
 
 export default LetterViewContainer;
+
+const Report = styled.button`
+  right: 20px;
+  bottom: 20px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  position: fixed;
+  opacity: 1;
+  color: white;
+  background-color: navy;
+  z-index: 100;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+`;
