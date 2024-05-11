01b486f3ddeb53dd2649599c7d37d19afe12edb2
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-11 15:37:13 +0900

    refactor: 공통 TextParser 컴포넌트 생성 및 적용

--- frontend/components/danfesta/EventDescription.tsx
+++ frontend/components/danfesta/EventDescription.tsx
@@ -1,8 +1,8 @@
-import React, { Fragment, useEffect } from "react";
-import uuid from "react-uuid";
+import React from "react";
 import BackgroundTemplate from "../slow/BackgroundTemplate";
 import styled from "styled-components";
 import dynamic from "next/dynamic";
+import TextParser from "../../common/TextParser";
 
 const DankookStudentCouncilLogin = dynamic(
   () => import("dankook-student-council-login"),
@@ -10,33 +10,17 @@ const DankookStudentCouncilLogin = dynamic(
     ssr: false,
   }
 );
-interface Props {
-  unmountHandler: () => void;
-}
-const EventDescription = ({ unmountHandler }: Props) => {
-  useEffect(() => {
-    return () => {
-      unmountHandler();
-    };
-  }, [unmountHandler]);
-
-  const enter = (paragraph: string) => {
-    return paragraph.split("\n").map((sentence) => (
-      <Fragment key={uuid()}>
-        {sentence}
-        <br />
-      </Fragment>
-    ));
-  };
-
+const EventDescription = () => {
   return (
     <BackgroundTemplate imageUrl={"/static/images/danfesta-background.jpeg"}>
       <TextContainer>
         <Head>랑데부: 만남의 우편함</Head>
         <Sub>
-          {enter(
-            "익명의 학우에게 편지를 남겨보세요.\n편지 하나를 남기면 세 장의 편지를 읽을 수 있어요.\n편지를 남긴 사람이 궁금하다면,\n편지에 적혀있는 연락처로 만남을 추진해요.\n당신의 새로운 만남을 응원합니다 ᰔ\n\n˖◛⁺˖♡ﾟ･♪⁺\n본 이벤트는 5월 21일부터 23일까지 3일 간 운영됩니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n총학생회 계정으로 로그인한 단국대학교 학생만\n글을 남길 수 있습니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n연락처를 남기는 건 자유입니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n열람한 편지는 마이페이지에서 볼 수 있습니다."
-          )}
+          <TextParser
+            text={
+              "익명의 학우에게 편지를 남겨보세요.\n편지 하나를 남기면 세 장의 편지를 읽을 수 있어요.\n편지를 남긴 사람이 궁금하다면,\n편지에 적혀있는 연락처로 만남을 추진해요.\n당신의 새로운 만남을 응원합니다 ᰔ\n\n˖◛⁺˖♡ﾟ･♪⁺\n본 이벤트는 5월 21일부터 23일까지 3일 간 운영됩니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n총학생회 계정으로 로그인한 단국대학교 학생만\n글을 남길 수 있습니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n연락처를 남기는 건 자유입니다.\n\n˖◛⁺˖♡ﾟ･♪⁺\n열람한 편지는 마이페이지에서 볼 수 있습니다."
+            }
+          />
         </Sub>
       </TextContainer>
 
