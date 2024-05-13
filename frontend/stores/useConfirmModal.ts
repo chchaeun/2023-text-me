02970741a714ec74ee38a79e04b93123da428986
chchaeun <kce3615@naver.com>
2024-05-13 14:41:51 +0900

    feat: ConfirmModal 버튼 텍스트 변경

--- frontend/stores/useConfirmModal.ts
+++ frontend/stores/useConfirmModal.ts
@@ -1,28 +1,40 @@
 import { create } from "zustand";
 
+interface OpenModalProps{
+  content: string;
+  yesButtonText?: string;
+  noButtonText?: string;
+}
+
 interface ConfirmModal {
-  text: string | null;
-  clickYes: (value: unknown) => void;
-  clickNo: (value: unknown) => void;
-  openConfirmModal: ({ text }: { text: string }) => Promise<boolean>;
+  content: string;
+  yesButtonText: string;
+  noButtonText: string;
+  clickYesButton: (value: unknown) => void;
+  clickNoButton: (value: unknown) => void;
+  openConfirmModal: ({ content, yesButtonText, noButtonText }: OpenModalProps) => Promise<boolean>;
 }
 
 const useConfirmModal = create<ConfirmModal>((set, get) => ({
-  text: null,
-  clickYes: null,
-  clickNo: null,
-  openConfirmModal: ({ text }) => {
+  content: null,
+  yesButtonText: null,
+  noButtonText: null,
+  clickYesButton: null,
+  clickNoButton: null,
+  openConfirmModal: ({ content, yesButtonText = "확인", noButtonText = "취소" }) => {
+    set({ content, yesButtonText, noButtonText });
+    
     const promise = new Promise((resolve, reject) => {
-      set({ text, clickYes: resolve, clickNo: reject });
+      set({ clickYesButton: resolve, clickNoButton: reject });
     });
 
     return promise.then(
       () => {
-        set({ text: null });
+        set({ content: null, yesButtonText: null, noButtonText: null });
         return true;
       },
       () => {
-        set({ text: null });
+        set({ content: null, yesButtonText: null, noButtonText: null });
         return false;
       }
     );
