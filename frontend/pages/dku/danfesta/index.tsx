db6d9fd3ec5354a73386b4f55cfe202e9266200d
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-21 23:58:51 +0900

    feat: 랜딩페이지 API 연동

--- frontend/pages/dku/danfesta/index.tsx
+++ frontend/pages/dku/danfesta/index.tsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 import DanfestaRoom from "../../../components/danfesta/Room";
 import EventDescription from "../../../components/danfesta/EventDescription";
+import { useLetters } from "../../../stores/dku/danfesta/useLetters";
 
 function Danfesta() {
-  const isDkuUser = false;
-  const [process, setProcess] = useState<"ROOM" | "DESCRIPTION">("ROOM");
+  const [process, setProcess] = useState<"ROOM" | "DESCRIPTION">();
+
+  const { getLetters } = useLetters();
 
   useEffect(() => {
-    if (!isDkuUser) {
-      setProcess("DESCRIPTION");
-    }
-  }, [isDkuUser]);
+    getLetters(
+      { gender: null },
+      () => setProcess("ROOM"),
+      () => setProcess("DESCRIPTION")
+    );
+  }, []);
 
   switch (process) {
     case "ROOM":
       return <DanfestaRoom />;
 
     case "DESCRIPTION":
-      return <EventDescription />;
+      return <EventDescription loginCallback={() => setProcess("ROOM")} />;
+
+    default:
+      return <></>;
   }
 }
 
