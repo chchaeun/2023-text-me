04cc0b8f19521ba0a610ef78b676f98a2a3cd63f
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-22 00:10:49 +0900

    feat: og 값 추가

--- frontend/pages/dku/danfesta/index.tsx
+++ frontend/pages/dku/danfesta/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import DanfestaRoom from "../../../components/danfesta/Room";
 import EventDescription from "../../../components/danfesta/EventDescription";
 import { useLetters } from "../../../stores/dku/danfesta/useLetters";
+import Head from "next/head";
 
 function Danfesta() {
   const [process, setProcess] = useState<"ROOM" | "DESCRIPTION">();
@@ -16,16 +17,35 @@ function Danfesta() {
     );
   }, []);
 
-  switch (process) {
-    case "ROOM":
-      return <DanfestaRoom />;
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content="static/images/danfesta.background.png"
+        />
+        <meta
+          property="og:description"
+          content="따뜻한 편지 한 통으로 소중한 인연을 만들어보세요!"
+        />
+        <meta property="og:title" content="랑데부: 만남의 우편함" />
+      </Head>
+      {(() => {
+        switch (process) {
+          case "ROOM":
+            return <DanfestaRoom />;
 
-    case "DESCRIPTION":
-      return <EventDescription loginCallback={() => setProcess("ROOM")} />;
+          case "DESCRIPTION":
+            return (
+              <EventDescription loginCallback={() => setProcess("ROOM")} />
+            );
 
-    default:
-      return <></>;
-  }
+          default:
+            return <></>;
+        }
+      })()}
+    </>
+  );
 }
 
 export default Danfesta;
